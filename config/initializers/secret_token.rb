94980b1d19e09618d50c7f0b7e08fac43c748b26
Cedric Winbush <cwvisionnfocus@gmail.com>
2013-02-18 13:50:04 -0500

    trial 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PIManager::Application.config.secret_token = '91ed8d627cd9a8b4d0c12554340f1bea06f15396979f5af18e06a785f931bbf241e31c0b22c9361d55df35c78443b2ad849c97309dc71ed426cfb6a8e52d094e'
